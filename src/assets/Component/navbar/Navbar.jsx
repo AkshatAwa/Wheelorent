0bf9f4e07e8307b2845f336dded796659f801922
AkshatAwasthi <137002607+AkshatAwa@users.noreply.github.com>
2025-02-20 14:27:57 +0530

    Trial

--- src/assets/Component/navbar/Navbar.jsx
+++ src/assets/Component/navbar/Navbar.jsx
@@ -1,11 +1,8 @@
 import React from "react";
-import { useAuth0 } from "@auth0/auth0-react";
 import "./navbar.css";
 import logo from "./wor.png"; // Import logo image
 
 const Navbar = () => {
-  const { loginWithRedirect, isAuthenticated, logout, user } = useAuth0();
-
   return (
     <nav className="navbar">
       {/* Logo */}
@@ -15,37 +12,16 @@ const Navbar = () => {
 
       {/* Navigation Links */}
       <ul className="nav-links">
-        <li>
-          <a href="#">Become a renter</a>
-        </li>
-        <li>
-          <a href="#">Rental deals</a>
-        </li>
-        <li>
-          <a href="#">How it works</a>
-        </li>
-        <li>
-          <a href="#">Why choose us</a>
-        </li>
+        <li><a href="#">Become a renter</a></li>
+        <li><a href="#">Rental deals</a></li>
+        <li><a href="#">How it works</a></li>
+        <li><a href="#">Why choose us</a></li>
       </ul>
 
-      {/* LogIn & LogOut Buttons */}
+      {/* Right Side (Sign In & Sign Up) */}
       <div className="auth-buttons">
-        <p>{user?.picture}</p>
-        {isAuthenticated ? (
-          <button
-            className="Log-out"
-            onClick={() =>
-              logout({ logoutParams: { returnTo: window.location.origin } })
-            }
-          >
-            Log Out
-          </button>
-        ) : (
-          <button className="Log-in" onClick={() => loginWithRedirect()}>
-            Log In
-          </button>
-        )}
+        <a href="#" className="sign-in">Sign in</a>
+        <a href="#" className="sign-up">Sign up</a>
       </div>
     </nav>
   );
