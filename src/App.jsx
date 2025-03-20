9e98cbf3aa5714d5ddaa32aeb7cbb1cfe0b3abf2
AkshatAwasthi <137002607+AkshatAwa@users.noreply.github.com>
2025-03-20 14:16:01 +0530

    Routes Assigned to navigation

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './assets/Component/navbar/Navbar'
 import Mainhero from './assets/Component/MainHero/Mainhero'
+import PublishCar from './assets/Component/PublishCar/PublishCar'
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Mainhero />} />
+        <Route path="/PublishyourVehicle" element={<PublishCar />} />
       </Routes>
     </Router>
     </>
