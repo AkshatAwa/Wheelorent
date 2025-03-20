eede82f8fe65d2f5ff89803edb2ea789968b431e
AkshatAwasthi <137002607+AkshatAwa@users.noreply.github.com>
2025-03-20 14:10:59 +0530

    Created Routes

---
+++ src/assets/Component/PublishCar/PublishCar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import HowItWorks from '../HowItWork/Hiw'
+import RentalDeals from '../RentDeals/RentDeals'
+import WhyChooseUs from '../WhyChooseUs/Wcu'
+import FAQ from '../FAQ/Faq'
+import Footer from '../Footer/Footer'
+
+const PublishCar = () => {
+  return (
+    <>
+    <HowItWorks/>
+    <RentalDeals/>
+    <RentalDeals/>
+    <WhyChooseUs/>
+    <FAQ/>
+    <Footer/>
+    </>
+  )
+}
+
+export default PublishCar
