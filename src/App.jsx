ddc3b092d41bf736960c41de2a811a7485753334
AkshatAwasthi <137002607+AkshatAwa@users.noreply.github.com>
2025-03-20 14:03:58 +0530

    Made New File

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,14 @@
 import react from 'react'
 import './App.css'
-import HowItWorks from './assets/Component/HowItWork/Hiw'
-import WhyChooseUs from './assets/Component/WhyChooseUs/Wcu'
-import FAQ from './assets/Component/FAQ/Faq'
-import Footer from './assets/Component/Footer/Footer'
 import Navbar from './assets/Component/navbar/Navbar'
-import RentalDeals from './assets/Component/RentDeals/RentDeals'
-import Hero from './assets/Component/Hero/Hero'
+import Mainhero from './assets/Component/MainHero/Mainhero'
 
 function App() {
 
   return (
     <>
      <Navbar/>
-     <Hero/>
-     <HowItWorks/>
-     <RentalDeals/>
-     <WhyChooseUs/>
-     <FAQ/>
-     <Footer/>
+     <Mainhero/>
     </>
   )
 }
