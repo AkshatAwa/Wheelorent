41fe0ca7a42aaba4225c4738b5a61494e00b1e57
AkshatAwasthi <137002607+AkshatAwa@users.noreply.github.com>
2025-03-21 00:04:28 +0530

    FormFinalized

--- src/assets/Component/PublishForm/PublishForm.jsx
+++ src/assets/Component/PublishForm/PublishForm.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import PublishVehicleForm from './Billing/Billing'
 
 const PublishForm = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <PublishVehicleForm/>
+    </>
   )
 }
 
